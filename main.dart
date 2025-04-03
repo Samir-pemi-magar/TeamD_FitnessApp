c5428493f0eb53cb0e941be80ed65e823abc09e9
Alish28 <np03cs4a230272@heraldcollege.edu.np>
2025-04-03 21:11:56 +0545

    Initial commit

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:fitnessapp/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/common/welcome_screen.dart';
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ZenFit',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WelcomeScreen(),
+    );
+  }
+}
